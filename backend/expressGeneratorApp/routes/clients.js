53b5d505b4b927b4397467d425c134e65bbc99f7
jafidiesel <jafibravin@outlook.com>
2020-05-04 10:58:41 -0300

    basic ok structure

--- backend/expressGeneratorApp/routes/clients.js
+++ backend/expressGeneratorApp/routes/clients.js
@@ -1,40 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var helper = require('../helpers');
 
 /* GET clients listing. */
 router
-	.get('/', function(req, res, next) {
-		res.send('GET clients');
+	.get('/', function (req, res, next) {
+		res.send(helper.buildResponse(200, 'all ok', { name: "juan" }));
 	})
-	.post('/', function(req,res,next){
-		console.log(req.body);
-		res.send('POST client'+JSON.stringify(req.body));
+	.post('/', function (req, res, next) {
+		res.send(helper.buildResponse(200, 'client created', req.body));
 	})
-	.get('/:idClient', function(req,res,next){
-		res.send(`GET client with idClient = ${req.params.idClient}`)
+	.get('/:idClient', function (req, res, next) {
+		res.send(helper.buildResponse(200, 'all ok', { idClient: `${req.params.idClient}` }))
 	})
-	.put('/:idClient',function(req,res,next){
-        res.send(`PUT client with idClient = ${req.params.idClient} ${JSON.stringify(req.body)}` )
-    })
-    .delete('/:idClient',function(req,res,next){
-        res.send(`DELETE client with idClient = ${req.params.idClient}`)
+	.put('/:idClient', function (req, res, next) {
+		res.send(helper.buildResponse(200, 'client updated', req.body))
 	})
-	.get('/:idClient/contracts', function(req,res,next){
-		res.send(`GET contracts of client with idClient = ${req.params.idClient}`);
+	.delete('/:idClient', function (req, res, next) {
+		res.send(helper.buildResponse(200, 'client deleted', {}))
 	})
-	.post('/:idClient/contracts',function(req,res,next){
-        res.send('POST contract' + JSON.stringify(req.body));
+	.get('/:idClient/contracts', function (req, res, next) {
+		res.send(helper.buildResponse(200, 'all ok', { idClient: `${req.params.idClient}`}))
 	})
-	.get('/:idClient/contracts/:idContract', function(req,res,next){
-		res.send(`GET contract with id ${req.params.idContract} client with idClient = ${req.params.idClient}`)
+	.post('/:idClient/contracts', function (req, res, next) {
+		res.send(helper.buildResponse(200, 'contract created', req.body))
 	})
-	.put('/:idClient/contracts/:idContract',function(req,res,next){
-        res.send(`PUT contract with id ${req.params.idContract} client with idClient = ${req.params.idClient} ${JSON.stringify(req.body)}` )
-    })
-    .delete('/:idClient/contracts/:idContract',function(req,res,next){
-        res.send(`DELETE contract with id ${req.params.idContract} client with idClient = ${req.params.idClient}`)
+	.get('/:idClient/contracts/:idContract', function (req, res, next) {
+		res.send(helper.buildResponse(200, 'all ok', { idContract: `${req.params.idContract}`}))
 	})
-    
+	.put('/:idClient/contracts/:idContract', function (req, res, next) {
+		res.send(helper.buildResponse(200, 'contract updated', req.body ))
+	})
+	.delete('/:idClient/contracts/:idContract', function (req, res, next) {
+		res.send(helper.buildResponse(200, 'contract deleted', {} ))
+	})
+
 
 
 module.exports = router;
